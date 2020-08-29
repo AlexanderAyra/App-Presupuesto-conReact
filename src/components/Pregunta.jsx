a732cff79e461e4e4a5a6950e90118231ff2c329
Alexander <alexander.ayras@gmail.com>
2020-08-28 19:37:23 -0500

    Leyendo el presupuesto del Usuario

--- src/components/Pregunta.jsx
+++ src/components/Pregunta.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Pregunta = () => {
+	const [cantidad, setCantidad] = useState(0)
+
+	const handleChange = (e) => {
+		setCantidad(parseInt(e.target.value, 10))
+	}
+
+	const addPresupuesto = (e) => {
+		e.preventDefault()
+
+		// Validar
+
+		// si se pasa la validacion
+	}
+
 	return (
 		<div>
 			<h2>Coloca tu Presupuesto</h2>
-			<form>
+			<form onSubmit={addPresupuesto}>
 				<input
 					type='number'
 					className='u-full-width'
 					placeholder='Coloca tu presupuesto'
+					onChange={handleChange}
 				/>
 
 				<input
