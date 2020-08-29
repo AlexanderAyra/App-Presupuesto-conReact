ba8c66059e291cb28301b111413d511d59b57628
Alexander <alexander.ayras@gmail.com>
2020-08-28 19:59:21 -0500

    Crear Formulario de Gastos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import Pregunta from './components/Pregunta'
+import Formulario from './components/Formulario'
 
 function App() {
 	const [presupuesto, setPresupuesto] = useState(0)
@@ -11,6 +12,13 @@ function App() {
 				<h1>Gasto Semanal</h1>
 				<div className='contenido-principal contenido'>
 					<Pregunta setPresupuesto={setPresupuesto} setRestante={setRestante} />
+
+					<div className='row'>
+						<div className='one-half column'>
+							<Formulario />
+						</div>
+						<div className='one-half column'>2</div>
+					</div>
 				</div>
 			</header>
 		</div>
